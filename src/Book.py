59c6caf7dda51df81d51720a7e3384c351b57ab9
FerCesio <162721896+FerCesio@users.noreply.github.com>
2024-09-12 22:50:00 -0300

    Update Book.py

--- src/Book.py
+++ src/Book.py
@@ -2,42 +2,43 @@
 
 
 class Book:
-    def __init__(self, isbn: str, title: str, author: str, available: bool =True, checkout_num: int =0):
-        self.__isbn = isbn
-        self.__title = title
-        self.__author = author
-        self.__available = available
-        self.__checkout_num = checkout_num
+    def __init__(self, isbn: str, title: str, author: str, available: bool = True, checkout_num: int = 0) -> None:
+        self.__isbn: str = isbn
+        self.__title: str = title
+        self.__author: str = author
+        self.__available: bool = available
+        self.__checkout_num: int = checkout_num
 
     # Getters
-    def get_isbn(self):
+    def get_isbn(self) -> str:
         return self.__isbn
 
-    def get_title(self):
+    def get_title(self) -> str:
         return self.__title
 
-    def get_author(self):
+    def get_author(self) -> str:
         return self.__author
 
-    def is_available(self):
+    def is_available(self) -> bool:
         return self.__available
 
-    def get_checkout_num(self):
+    def get_checkout_num(self) -> int:
         return self.__checkout_num
 
     # Setters
-    def set_available(self, available: bool):
+    def set_available(self, available: bool) -> None:
         self.__available = available
 
-    def increment_checkout_num(self):
+    def increment_checkout_num(self) -> None:
         self.__checkout_num += 1
-
 
     # Utils
     def __str__(self) -> str:
-        return f"ISBN: {self.__isbn}, Title: {self.__title}, Author: {self.__author}, Available: {self.__available}, Checkout Number: {self.__checkout_num}"
+        return (f"ISBN: {self.__isbn}, Title: {self.__title}, "
+                f"Author: {self.__author}, Available: {self.__available}, "
+                f"Checkout Number: {self.__checkout_num}")
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if isinstance(other, Book):
             return (self.__isbn == other.__isbn and
                     self.__title == other.__title and
@@ -45,3 +46,4 @@ class Book:
                     self.__available == other.__available and
                     self.__checkout_num == other.__checkout_num)
         return False
+
